c1d71e8f9cfb3b5cfd19a1dbda584d4516c8501f
Dana-ochoa <eesmee866@gmail.com>
2022-12-02 23:37:53 -0600

    Modelos y Controladores

---
+++ app/Http/Controllers/Platillo_OrdenController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Platillo_Orden;
+use Illuminate\Http\Request;
+
+class Platillo_OrdenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Platillo_Orden  $platillo_Orden
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Platillo_Orden $platillo_Orden)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Platillo_Orden  $platillo_Orden
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Platillo_Orden $platillo_Orden)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Platillo_Orden  $platillo_Orden
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Platillo_Orden $platillo_Orden)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Platillo_Orden  $platillo_Orden
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Platillo_Orden $platillo_Orden)
+    {
+        //
+    }
+}
